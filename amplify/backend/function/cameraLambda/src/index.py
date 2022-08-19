2db7ad48f190725350ffc3877c894593a8272470
You Kai <kaiyour@amazon.com>
2022-08-19 10:04:03 +0800

    add camera get sync with panorama console

--- amplify/backend/function/cameraLambda/src/index.py
+++ amplify/backend/function/cameraLambda/src/index.py
@@ -3,94 +3,91 @@ import boto3
 import sys
 
 
-# SSM 
-env_p = boto3.client('ssm').get_parameter(Name='/ppe/env')['Parameter']['Value']
+# SSM
+env_p = boto3.client("ssm").get_parameter(Name="/ppe/env")["Parameter"]["Value"]
 
 # Hack to print to stderr so it appears in CloudWatch.
 def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, ** kwargs)
+    print(*args, file=sys.stderr, **kwargs)
 
 
 def post(event):
-    TABLE_NAME = 'Camera-' + env_p
-    db = boto3.resource('dynamodb')
-    pano_client = boto3.client('panorama')
+    TABLE_NAME = "Camera-" + env_p
+    db = boto3.resource("dynamodb")
+    pano_client = boto3.client("panorama")
     table = db.Table(TABLE_NAME)
-    body = json.loads(event['body'])
-    
-    CAMERA_NAME = body['brand']
+    body = json.loads(event["body"])
+
+    CAMERA_NAME = body["brand"]
     CAMERA_CREDS = {
-        "Username": body['location'],
-        "Password": body['network'],
-        "StreamUrl": body['address']
+        "Username": body["location"],
+        "Password": body["network"],
+        "StreamUrl": body["address"],
     }
 
-    print(body['camera_id'])
-    print(body['address'])
-    print(body['description'])
-    print(body['location'])
-    print(body['brand'])
-    print(body['network'])
-    print(body['image_size'])
-    
+    print(body["camera_id"])
+    print(body["address"])
+    print(body["description"])
+    print(body["location"])
+    print(body["brand"])
+    print(body["network"])
+    print(body["image_size"])
+
     try:
         pano_res = pano_client.create_node_from_template_job(
             NodeName=CAMERA_NAME,
             OutputPackageName=CAMERA_NAME,
-            OutputPackageVersion='0.1',
+            OutputPackageVersion="0.1",
             TemplateParameters=CAMERA_CREDS,
-            TemplateType='RTSP_CAMERA_STREAM'
+            TemplateType="RTSP_CAMERA_STREAM",
         )
     except Exception as e:
-        eprint('Error !!')
+        eprint("Error !!")
         eprint(e)
         return {
-            'statusCode': 404,
-            'body': 'Camera Error',
-            'headers': {
-                 "Access-Control-Allow-Headers" : "*",
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': '*',
-            }
+            "statusCode": 404,
+            "body": "Camera Error",
+            "headers": {
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*",
+            },
         }
 
     try:
         response = table.put_item(
             Item={
-                'camera_id': body['camera_id'],
-                'address': body['address'],
-                'description': body['description'],
-                'location': body['location'],
-                'brand': body['brand'],
-                'network': body['network'],
-                'image_size': body['image_size'],
-                'JobId': pano_res['JobId'],
+                "camera_id": body["camera_id"],
+                "address": body["address"],
+                "description": body["description"],
+                "location": body["location"],
+                "brand": body["brand"],
+                "network": body["network"],
+                "image_size": body["image_size"],
+                "JobId": pano_res["JobId"],
             }
         )
-        eprint('OK !!')
+        eprint("OK !!")
         eprint(response)
         return {
-            'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
-            'body': body['camera_id'],
-            'headers': {
-                 "Access-Control-Allow-Headers" : "*",
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': '*',
-            }
+            "statusCode": response["ResponseMetadata"]["HTTPStatusCode"],
+            "body": body["camera_id"],
+            "headers": {
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*",
+            },
         }
     except Exception as e:
         # raise e
-        eprint('Error !!')
+        eprint("Error !!")
         eprint(e)
-        return {
-            'statusCode': 500,
-            'body': json.dumps('error')
-        }
+        return {"statusCode": 500, "body": json.dumps("error")}
 
 
 def get(event):
     print(event)
-    TABLE_NAME = 'Camera-' + env_p
+    TABLE_NAME = "Camera-" + env_p
     eprint(">>> Start query config.")
     # return {
     #   'statusCode': 200,
@@ -102,47 +99,45 @@ def get(event):
     #   'body': json.dumps('Hello from your new Amplify Python lambda!')
     # }
     result = ""
-    db = boto3.resource('dynamodb')
-    pano_client = boto3.client('panorama')
+    db = boto3.resource("dynamodb")
+    panorama_client = boto3.client("panorama")
     table = db.Table(TABLE_NAME)
 
     try:
-        # response = pano_client.list_node_from_template_jobs()
-        # # response = table.scan()
-        # eprint(response)
-        # eprint(response['ResponseMetadata'])
-        # eprint(response['NodeFromTemplateJobs'])
-        # return {
-        #     'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
-        #     'body': json.dumps(response['NodeFromTemplateJobs'],default=str),
-        #     'headers': {
-        #         "Access-Control-Allow-Headers" : "*",
-        #         'Access-Control-Allow-Origin': '*',
-        #         'Access-Control-Allow-Headers': '*',
-        #     }
-        # }
-        response = table.scan()
-        eprint(response['ResponseMetadata']['HTTPStatusCode'])
-        eprint(response['Items'])
+        response = panorama_client.list_nodes()
+        cameras = []
+        for node in response["Nodes"]:
+            camera = {}
+            camera["NodeId"] = node["NodeId"]
+            camera["Name"] = node["Name"]
+            camera["CreatedTime"] = node["CreatedTime"].strftime("%Y/%m/%d, %H:%M:%S")
+            eprint(node["NodeId"])
+            eprint(node["Name"])
+            if "Description" in node:
+                camera["Description"] = node["Description"]
+                eprint(node["Description"])
+            eprint(node["CreatedTime"].strftime("%Y/%m/%d, %H:%M:%S"))
+            cameras.append(camera)
+
+        # response = table.scan()
+        # eprint(response["ResponseMetadata"]["HTTPStatusCode"])
+        # eprint(response["Items"])
         return {
-            'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
-            'body':  json.dumps(response['Items']),
-            'headers': {
-                "Access-Control-Allow-Headers" : "*",
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': '*',
-            }
+            "statusCode": response["ResponseMetadata"]["HTTPStatusCode"],
+            "body": json.dumps(cameras),
+            "headers": {
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*",
+            },
         }
     except Exception as e:
         eprint(e)
-        return {
-            'statusCode': 500,
-            'body': 'Error!!'
-        }
+        return {"statusCode": 500, "body": "Error!!"}
 
 
 def handler(event, context):
-    if event['httpMethod'] == 'POST':
+    if event["httpMethod"] == "POST":
         return post(event)
-    elif event['httpMethod'] == 'GET':
+    elif event["httpMethod"] == "GET":
         return get(event)
--- src/components/CameraCfgTable.js
+++ src/components/CameraCfgTable.js
@@ -6,42 +6,42 @@ import StatusIndicator from 'aws-northstar/components/StatusIndicator';
 import Button from 'aws-northstar/components/Button';
 import Inline from 'aws-northstar/layouts/Inline';
 
-import React  from 'react';
-import { connect } from 'react-redux' 
+import React from 'react';
+import { connect } from 'react-redux'
 
 import axios from 'axios'
 import Amplify, { API } from 'aws-amplify';
 
-import {withTranslation} from 'react-i18next'
+import { withTranslation } from 'react-i18next'
 
 const mapStateToProps = state => {
-  return { session: state.session }
+    return { session: state.session }
 }
 
 const MapDispatchTpProps = (dispatch) => {
-  return {
-      changeLang: (key)=>dispatch({type: 'change_language',data: key})
-  }
+    return {
+        changeLang: (key) => dispatch({ type: 'change_language', data: key })
+    }
 }
 
 const columnDefinitions = [
     {
-        'id': 'camera_id',
+        'id': 'NodeId',
         width: 300,
         Header: 'ID',
-        accessor: 'camera_id'
+        accessor: 'NodeId'
     },
     {
-        'id': 'address',
+        'id': 'Name',
         width: 600,
-        Header: 'Address',
-        accessor: 'address'
+        Header: 'Camera Name',
+        accessor: 'Name'
     },
     {
-        'id': 'description',
+        'id': 'Description',
         width: 300,
         Header: 'Description',
-        accessor: 'description'
+        accessor: 'Description'
     },
     // {
     //     'id': 'location',
@@ -50,10 +50,10 @@ const columnDefinitions = [
     //     accessor: 'location'
     // },
     {
-        'id': 'brand',
+        'id': 'CreatedTime',
         width: 200,
         Header: 'Name',
-        accessor: 'brand'
+        accessor: 'CreatedTime'
     },
     // {
     //     'id': 'network',
@@ -69,89 +69,89 @@ const columnDefinitions = [
     // }
 ]
 
-class  CameraCfgTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        loading : true ,
-        job_list : [],
-        curent : {}
-    }
-  }
-
-  componentDidMount(){
-    // console.log(this.model_list)
-    this.setState({loading:true},()=>{
-        this.load_data()
-    })
-  }
-
-  async load_data(){
-    await API.get('backend','/camera').then(res => {
-        console.log('return Data',res)
-        if (res){ 
-            console.log(res)
-            var _tmp_data = []
-            res.forEach((item)=>{
-                var _tmp = {}
-                _tmp['camera_id'] = item['camera_id']
-                _tmp['address'] = item['address']
-                _tmp['description'] = item['description']
-                _tmp['location'] = item['location']
-                _tmp['brand'] = item['brand']
-                _tmp['network'] = item['network']
-                _tmp['image_size'] = item['image_size']    
-                _tmp_data.push(_tmp)
-
-            });
-            this.setState({job_list:_tmp_data},()=>{
-            })
-            this.setState({loading:false})
+class CameraCfgTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            job_list: [],
+            curent: {}
         }
-        // console.log(this.state.model_list)
-        return res
-    })
-  }
+    }
 
-  jump_to_newCfg(){
-    this.props.history.push("/NewCameraConfig")
-  }
+    componentDidMount() {
+        // console.log(this.model_list)
+        this.setState({ loading: true }, () => {
+            this.load_data()
+        })
+    }
+
+    async load_data() {
+        await API.get('backend', '/camera').then(res => {
+            console.log('return Data', res)
+            if (res) {
+                console.log(res)
+                var _tmp_data = []
+                res.forEach((item) => {
+                    var _tmp = {}
+                    _tmp['NodeId'] = item['NodeId']
+                    _tmp['Name'] = item['Name']
+                    _tmp['Description'] = item['Description']
+                    _tmp['CreatedTime'] = item['CreatedTime']
+                    // _tmp['brand'] = item['brand']
+                    // _tmp['network'] = item['network']
+                    // _tmp['image_size'] = item['image_size']    
+                    _tmp_data.push(_tmp)
+
+                });
+                this.setState({ job_list: _tmp_data }, () => {
+                })
+                this.setState({ loading: false })
+            }
+            // console.log(this.state.model_list)
+            return res
+        })
+    }
+
+    jump_to_newCfg() {
+        this.props.history.push("/NewCameraConfig")
+    }
 
 
 
-  render(){
-    const {
-        props: {t}
-    } = this;
+    render() {
+        const {
+            props: { t }
+        } = this;
 
 
-    const tableActions = (
-        <Inline>
-            <Button variant="primary" onClick={() => this.jump_to_newCfg()}>
-                {t('New Camera Config')}
-            </Button>
-        </Inline>
-    );
+        const tableActions = (
+            <Inline>
+                <Button variant="primary" onClick={() => this.jump_to_newCfg()}>
+                    {t('New Camera Config')}
+                </Button>
+            </Inline>
+        );
 
-    return(
+        return (
 
-        <Table
-            id = "CameraCfgTable"
-            actionGroup={tableActions}
-            tableTitle={t('Camera Config')}
-            multiSelect={false}
-            columnDefinitions={columnDefinitions}
-            items={this.state.job_list}
-            onSelectionChange={(item)=>{this.setState({curent:item})}}
-            // getRowId={this.getRowId}
-            loading={this.state.loading}
-            disableSettings={false}
+            <Table
+                id="CameraCfgTable"
+                actionGroup={tableActions}
+                tableTitle={t('Camera Config')}
+                multiSelect={false}
+                columnDefinitions={columnDefinitions}
+                items={this.state.job_list}
+                onSelectionChange={(item) => { this.setState({ curent: item }) }}
+                // getRowId={this.getRowId}
+                loading={this.state.loading}
+                disableSettings={false}
             // onFetchData={this.handleFetchData}
-        />
-    )
-  }
+            />
+        )
+    }
 }
 
 
-export default connect(mapStateToProps,MapDispatchTpProps)(withTranslation()(CameraCfgTable));
+export default connect(mapStateToProps, MapDispatchTpProps)(withTranslation()(CameraCfgTable));
 
